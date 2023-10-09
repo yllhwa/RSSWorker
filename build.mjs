1cf8944c3afc9eefd6d00fb45b9a28618b416ebe
yllhwa <yll2002mail@gmail.com>
2023-10-09 23:27:05 +0800

    自定义构建

---
+++ build.mjs
@@ -0,0 +1,42 @@
+import esbuild from 'esbuild';
+import fs from 'fs/promises';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+
+function buildWorker({ entry, out, debug, external } = {}) {
+	return esbuild.build({
+		plugins: [
+			NodeModulesPolyfillPlugin(),
+		],
+		platform: 'browser',
+		conditions: ['worker', 'browser'],
+		entryPoints: [entry],
+		sourcemap: true,
+		outfile: out,
+		external,
+		logLevel: 'warning',
+		format: 'esm',
+		target: 'es2022',
+		bundle: true,
+		minify: !debug,
+		define: {
+			IS_CLOUDFLARE_WORKER: 'true',
+		},
+		loader: {
+			'.html': 'text',
+			'.css': 'text',
+			'.txt': 'text',
+		},
+		metafile: true,
+	});
+}
+
+let result = await buildWorker({
+	entry: './src/worker.js',
+	out: './dist/worker.js',
+	debug: false,
+});
+
+if (result.metafile) {
+	// use https://esbuild.github.io/analyze/ to analyses
+	await fs.writeFile('./dist/metafile.json', JSON.stringify(result.metafile));
+}
