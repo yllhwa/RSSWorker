94d9eab3705892fc3d7b022f5ca561e6a3398569
yllhwa <yll2002mail@gmail.com>
2023-10-11 00:25:11 +0800

    修复telegram部分问题

--- src/lib/telegram/channel.js
+++ src/lib/telegram/channel.js
@@ -3,13 +3,14 @@ import { renderRss2 } from '../../utils/util';
 let deal = async (ctx) => {
 	const { username } = ctx.req.param();
 	let res = await fetch(`https://t.me/s/${username}`);
-	const rewriter = new HTMLRewriter();
 	let title = '';
 	let link = `https://t.me/s/${username}`;
 	let description = '';
 	let language = 'zh-cn';
 	let tgme_widget_message_texts = [];
 	let tgme_widget_message_dates = [];
+	let src_links = [];
+	let last_tag = '';
 	let new_res = new HTMLRewriter()
 		.on('head > title', {
 			text(text) {
@@ -21,7 +22,7 @@ let deal = async (ctx) => {
 				description += element.getAttribute('content');
 			},
 		})
-		.on('.tgme_widget_message_text', {
+		.on('.tgme_widget_message_bubble > .tgme_widget_message_text', {
 			element(element) {
 				tgme_widget_message_texts.push('');
 			},
@@ -29,26 +30,50 @@ let deal = async (ctx) => {
 				tgme_widget_message_texts[tgme_widget_message_texts.length - 1] += text.text;
 			},
 		})
+		.on('.tgme_widget_message_bubble > .tgme_widget_message_text > *', {
+			element(element) {
+				// add <br> tag
+				tgme_widget_message_texts[tgme_widget_message_texts.length - 1] += '<br>';
+			},
+			text(text) {
+				if (text.lastInTextNode) {
+					tgme_widget_message_texts[tgme_widget_message_texts.length - 1] += '<br>';
+				}
+			},
+		})
 		.on('.tgme_widget_message_date > time', {
 			element(element) {
 				tgme_widget_message_dates.push(element.getAttribute('datetime'));
 			},
 		})
+		.on('.tgme_widget_message_wrap > .tgme_widget_message', {
+			element(element) {
+				let data_post = element.getAttribute('data-post');
+				src_links.push(`https://t.me/${username}/${data_post}`);
+			},
+		})
 		.transform(res);
 	await new_res.text();
 	let items = [];
+	src_links = src_links.reverse();
+	tgme_widget_message_dates = tgme_widget_message_dates.reverse();
 	for (let i = 0; i < tgme_widget_message_texts.length; i++) {
 		if (tgme_widget_message_texts[i] === '') {
 			continue;
 		}
+		let title = tgme_widget_message_texts[i].replace(/<br>/g, ' ');
+		if (title.length > 100) {
+			title = title.slice(0, 100) + '...';
+		}
 		let item = {
-			title: tgme_widget_message_texts[i],
-			link: link,
+			title: title,
+			link: src_links.pop(),
 			description: tgme_widget_message_texts[i],
-			pubDate: tgme_widget_message_dates[i],
+			pubDate: tgme_widget_message_dates.pop(),
 		};
 		items.push(item);
 	}
+	items = items.reverse();
 	let data = {
 		title: title,
 		link: link,
--- src/route.js
+++ src/route.js
@@ -1,5 +1,4 @@
 import { Hono } from 'hono';
-import { HTTPException } from 'hono/http-exception';
 
 const route = new Hono();
 
