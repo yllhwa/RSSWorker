6a1613709424bd6fca103feed22d8088bfa75808
yllhwa <yll2002mail@gmail.com>
2025-10-28 20:10:25 +0000

    添加index.html和robots.txt

--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,20 @@
 import { Hono } from 'hono';
 import { basicAuth } from 'hono/basic-auth';
 import { cors } from 'hono/cors';
+import indexHtml from './index.html';
+import robotsTxt from './robots.txt';
 
 import route from './route';
 
 const app = new Hono();
 
 app.route('/rss', route);
-
+app.get('/', (ctx) => {
+	return ctx.html(indexHtml);
+});
+app.get('robots.txt', (ctx) => {
+	return ctx.text(robotsTxt);
+});
 // app.use(
 // 	'/*',
 // 	basicAuth({
